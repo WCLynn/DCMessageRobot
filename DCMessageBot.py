965f5b7f0c69cceb6b81750f5141ac9121c398b3
WCLynn <qq6710302000@gmail.com>
2025-03-29 17:21:03 +0800

    ADD Bot.py and requirements

---
+++ DCMessageBot.py
@@ -0,0 +1,31 @@
+
+import discord
+from discord.ext import commands
+import os
+
+# 設定機器人指令前綴
+intents = discord.Intents.all()
+bot = commands.Bot(command_prefix="!", intents=intents)
+
+
+@bot.event
+#當機器人完成啟動時
+async def on_ready():
+    game = discord.Game('裝忙中')
+    #discord.Status.<狀態>，可以是online,offline,idle,dnd,invisible
+    await bot.change_presence(status=discord.Status.idle, activity=game)
+    print(f"已登入為 {bot.user}")
+    
+@bot.event    
+async def on_message(message):
+    if message.content.startswith('$hello'):
+        await message.channel.send('Hello!')
+
+
+@bot.command()
+async def close(ctx):
+    """關閉機器人"""
+    await ctx.send("關閉機器人中")
+    await bot.close()
+    
+bot.run("HERE")
